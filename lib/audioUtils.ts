1daaf3ddffc1ddf2feb008b91d54cf940dd2d58a
Rakesh K <codersgyan@gmail.com>
2026-02-17 15:43:13 +0300

    play audio

--- lib/audioUtils.ts
+++ lib/audioUtils.ts
@@ -11,12 +11,12 @@ export function createPCMBlob(data: Float32Array) {
 
     int16[i] =
       element < 0 ? element * 32768 : element * 32767;
-
-    return {
-      data: arrayBufferToBase64(int16),
-      mimeType: "audio/pcm;rate=16000",
-    };
   }
+
+  return {
+    data: arrayBufferToBase64(int16),
+    mimeType: "audio/pcm;rate=16000",
+  };
 }
 
 function arrayBufferToBase64(data: Int16Array) {
@@ -28,4 +28,40 @@ function arrayBufferToBase64(data: Int16Array) {
   }
 
   return btoa(str);
+}
+
+export function base64ToUint8Array(
+  base64: string,
+): Uint8Array {
+  const binaryString = atob(base64);
+  const len = binaryString.length;
+  const bytes = new Uint8Array(len);
+  for (let i = 0; i < len; i++) {
+    bytes[i] = binaryString.charCodeAt(i);
+  }
+  return bytes;
+}
+
+export async function decodeAudioData(
+  data: Uint8Array,
+  ctx: AudioContext,
+  sampleRate: number,
+  numChannels: number,
+): Promise<AudioBuffer> {
+  const dataInt16 = new Int16Array(data.buffer);
+  const frameCount = dataInt16.length / numChannels;
+  const buffer = ctx.createBuffer(
+    numChannels,
+    frameCount,
+    sampleRate,
+  );
+
+  for (let channel = 0; channel < numChannels; channel++) {
+    const channelData = buffer.getChannelData(channel);
+    for (let i = 0; i < frameCount; i++) {
+      channelData[i] =
+        dataInt16[i * numChannels + channel] / 32768.0;
+    }
+  }
+  return buffer;
 }
