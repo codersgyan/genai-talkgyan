0d71a45c5aae5bf7dacb0b73a540fd24d59c9cc6
Rakesh K <codersgyan@gmail.com>
2026-02-20 15:06:27 +0300

    add ephemeral tokens

--- services/liveManager.ts
+++ services/liveManager.ts
@@ -39,10 +39,13 @@ export class LiveManager {
   private inputTranscription = "";
   private outputTranscription = "";
 
-  constructor(callbacks: LiveManagerCallbacks) {
+  constructor(
+    callbacks: LiveManagerCallbacks,
+    token: string,
+  ) {
     this.ai = new GoogleGenAI({
-      // IMPORTANT: don't use this in production... (will use ephemeral tokens)
-      apiKey: process.env.NEXT_PUBLIC_GEMINI_API_KEY,
+      apiKey: token,
+      apiVersion: "v1alpha",
     });
 
     this.callbacks = callbacks;
@@ -84,6 +87,7 @@ export class LiveManager {
           },
           onmessage: this.handleMessage.bind(this),
           onerror: (e) => {
+            console.log(e);
             this.callbacks.onStateChange(
               ConnectionState.ERROR,
             );
